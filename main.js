2dfd087b4a504915a7d4880127b14154b8f6324d
Sam Hodgkinson <55709069+microhod@users.noreply.github.com>
2024-07-10 15:59:41 +0100

    add fullscreen modal for mobile navigation

--- main.js
+++ main.js
@@ -23,6 +23,26 @@ function showContentFromHash() {
     showPage(pageId);
 }
 
+function showMobileModal() {
+    modal = document.querySelector('.mobile-nav-modal');
+    modal.classList.remove("modal-hidden");
+    modal.classList.add("modal-active");
+}
+
+function mobileNav(link) {
+    modal = document.querySelector('.mobile-nav-modal');
+    modal.classList.add("modal-hidden");
+
+    modal.addEventListener('animationend', () => {
+        console.log("remove active");
+        modal.classList.remove("modal-active");
+    }, {
+        once: true,
+    });
+
+    window.URL = link;
+}
+
 // Call the function on page load to display page content based on URL
 window.onload = showContentFromHash;
 // Call the function on hash change (for back/forward navigation)
